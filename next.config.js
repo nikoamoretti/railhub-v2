e400de60a551dd720bb6e6f4000cf6af5c8a6bfb
Nico Amoretti <nicoamoretti@gmail.com>
2026-02-18 13:40:48 -0800

    Add JSON webpack config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,14 @@ const nextConfig = {
     serverActions: {
       bodySizeLimit: '10mb'
     }
-  }
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.json$/,
+      type: 'json',
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
